35dc384211f34629d59252ff6dd4a155b526cf7b
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-02-04 00:18:44 -0500

    readd js

--- js/view-strategy.ts
+++ js/view-strategy.ts
@@ -7,10 +7,29 @@ import { LockCodeManagerEntitiesResponse, LockCodeManagerViewStrategyConfig } fr
 
 export class LockCodeManagerViewStrategy extends ReactiveElement {
   static async generate(config: LockCodeManagerViewStrategyConfig, hass: HomeAssistant) {
+    const { config_entry_id, config_entry_title } = config;
+    if (
+      (config_entry_id === undefined && config_entry_title === undefined) ||
+      (config_entry_id !== undefined && config_entry_title !== undefined)
+    ) {
+      return {
+        badges: [],
+        cards: [
+          {
+            content:
+              `## ERROR: Either \`config_entry_title\` or \`config_entry_id\` must ` +
+              `be provided in the view config, but not both!`,
+            type: 'markdown'
+          }
+        ],
+        title: 'Lock Code Manager'
+      };
+    }
     try {
       const [configEntryId, configEntryTitle, entities] =
         await hass.callWS<LockCodeManagerEntitiesResponse>({
-          config_entry_title: config.config_entry_title,
+          config_entry_id,
+          config_entry_title,
           type: 'lock_code_manager/get_config_entry_entities'
         });
       return generateView(
@@ -22,11 +41,15 @@ export class LockCodeManagerViewStrategy extends ReactiveElement {
         config.include_code_slot_sensors ?? DEFAULT_INCLUDE_CODE_SLOT_SENSORS
       );
     } catch (err) {
+      const content =
+        config_entry_id !== undefined
+          ? `with ID \`${config_entry_id}\``
+          : `called \`${config_entry_title}\``;
       return {
         badges: [],
         cards: [
           {
-            content: `# No Lock Code Manager configuration called \`${config.config_entry_title}\` found!`,
+            content: `## ERROR: No Lock Code Manager configuration ${content} found!`,
             type: 'markdown'
           }
         ],
