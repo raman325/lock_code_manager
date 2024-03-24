10904a513afcd2809a21d467b809514d625c363a
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-03-24 04:00:13 -0400

    Remove unnecessary test code

--- tests/zwave_js/test_provider.py
+++ tests/zwave_js/test_provider.py
@@ -1,18 +1,10 @@
 """Test the Z-Wave JS lock platform."""
 
-import pytest
-
 from homeassistant.core import HomeAssistant
 
 SCHLAGE_BE469_LOCK_ENTITY = "lock.touchscreen_deadbolt"
 
 
-async def test_door_lock(
-    hass: HomeAssistant,
-    client,
-    lock_schlage_be469,
-    integration,
-    caplog: pytest.LogCaptureFixture,
-) -> None:
+async def test_door_lock(hass: HomeAssistant) -> None:
     """Test a lock entity with door lock command class."""
     pass
