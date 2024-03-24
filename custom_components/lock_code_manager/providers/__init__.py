d462e1be3095d431f730a47508b9ff90201fe4bf
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-03-24 02:34:55 -0400

    fix mapping

--- custom_components/lock_code_manager/providers/__init__.py
+++ custom_components/lock_code_manager/providers/__init__.py
@@ -7,8 +7,10 @@ There should be one file per integration, named after the integration.
 from __future__ import annotations
 
 from ._base import BaseLock
+from .virtual import VirtualLock
 from .zwave_js import ZWaveJSLock
 
 INTEGRATIONS_CLASS_MAP: dict[str, type[BaseLock]] = {
+    "virtual": VirtualLock,
     "zwave_js": ZWaveJSLock,
 }
