1cd8972c2b4ba71014f268d246b1c1c4298f70d6
Raman Gupta <7243222+raman325@users.noreply.github.com>
2024-04-02 15:23:38 -0400

    Isolate backwards compat stuff to its own module

--- custom_components/lock_code_manager/backports.py
+++ custom_components/lock_code_manager/backports.py
@@ -5,11 +5,14 @@ from __future__ import annotations
 import logging
 from typing import Any
 
+from homeassistant.const import MAJOR_VERSION, MINOR_VERSION
 from homeassistant.core import Event
 
-from .const import EVENT_DATA_PASSED_IN
-
 _LOGGER = logging.getLogger(__name__)
+
+EVENT_DATA_PASSED_IN = MAJOR_VERSION > 2024 or (
+    MAJOR_VERSION == 2024 and MINOR_VERSION >= 4
+)
 
 
 def get_event_data_for_filter(evt: Event | dict[str, Any]) -> dict[str, Any]:
--- custom_components/lock_code_manager/const.py
+++ custom_components/lock_code_manager/const.py
@@ -2,14 +2,7 @@
 
 from __future__ import annotations
 
-from homeassistant.const import (
-    CONF_ENABLED,
-    CONF_NAME,
-    CONF_PIN,
-    MAJOR_VERSION,
-    MINOR_VERSION,
-    Platform,
-)
+from homeassistant.const import CONF_ENABLED, CONF_NAME, CONF_PIN, Platform
 
 DOMAIN = "lock_code_manager"
 VERSION = "0.0.0"  # this will be automatically updated as part of the release workflow
@@ -75,7 +68,3 @@ PLATFORM_MAP = {
     CONF_PIN: Platform.TEXT,
     EVENT_PIN_USED: Platform.EVENT,
 }
-
-EVENT_DATA_PASSED_IN = MAJOR_VERSION > 2024 or (
-    MAJOR_VERSION == 2024 and MINOR_VERSION >= 4
-)
